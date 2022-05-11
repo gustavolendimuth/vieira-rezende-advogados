49d864ed6fd893c22b1fac3dd7dbff04d9f313a9
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-05-10 21:01:30 -0300

    first commit

---
+++ vieira-advogados-profissionais.js
@@ -0,0 +1,16 @@
+const biosList = document.querySelector('.flex-bios');
+const posicao = document.querySelectorAll('#posicao');
+const positions = ['Consultor', 'Associado', 'Sócio'];
+
+const sortOrder = (positions) => {
+  for (let position of positions) {
+    for (let index = 0; index < posicao.length; index += 1) {
+      if (posicao[index].innerText === position) {
+        const posicaoNode = posicao[index].parentNode.parentNode;
+        biosList.prepend(posicaoNode);
+      }
+    }
+  }
+
+}
+sortOrder(positions);
